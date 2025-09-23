e9b38c293b69df43eb540b4da46e43cbf6221003
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-09-23 16:06:35 +0300

    Add initial Flask application structure with database and user model setup

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+# loading environmental variables
+
+load_dotenv()
+# print("ENV Credentials")
+# print(os.getenv("DATABASE_URL"))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY="secret"
+    JWT_SECRET_KEY="RandomStringForJWTSecretKey243!"
